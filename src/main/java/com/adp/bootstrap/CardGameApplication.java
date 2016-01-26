cfde4f52bb34db9a147d88aadafdcaed84120032
Adarsh Sharma <adarsh.sharma@flipkart.com>
2016-01-26 16:12:11 +0530

    working socket partially as sockets keep getting closed automatically

--- src/main/java/com/adp/bootstrap/CardGameApplication.java
+++ src/main/java/com/adp/bootstrap/CardGameApplication.java
@@ -4,7 +4,6 @@ import com.adp.config.CardGameConfiguration;
 import com.adp.filters.RequestFilter;
 import com.adp.filters.ResponseFilter;
 import com.adp.resources.CardGameResource;
-import com.adp.resources.atmosphere.SocketResource;
 import com.adp.utils.GuiceInjector;
 import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
@@ -15,9 +14,12 @@ import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.atmosphere.cpr.ApplicationConfig;
+import org.atmosphere.cpr.AtmosphereServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletRegistration;
 import javax.validation.Validator;
 
 /**
@@ -55,7 +57,6 @@ public class CardGameApplication extends Application<CardGameConfiguration> {
 
         environment.lifecycle().manage(injector.getInstance(CardGameManage.class));
         environment.jersey().register(injector.getInstance(CardGameResource.class));
-        environment.jersey().register(injector.getInstance(SocketResource.class));
 
 //        CoreRotationManagementTask managementTask = new CoreRotationManagementTask(config.getRotationManagementConfig());
 //        environment.admin().addTask(managementTask);
@@ -64,24 +65,20 @@ public class CardGameApplication extends Application<CardGameConfiguration> {
         environment.jersey().register(injector.getInstance(RequestFilter.class));
         environment.jersey().register(injector.getInstance(ResponseFilter.class));
 
-//        initializeAtmosphere(environment);
+        initializeAtmosphere(environment);
 
         JmxReporter.forRegistry(environment.metrics()).build().start();
         JmxReporter.forRegistry(metricRegistry).build().start();
     }
 
-//    void initializeAtmosphere(Environment environment) {
-//
-//        FilterBuilder config = environment.addFilter(CrossOriginFilter.class, "/socket");
-//        config.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-//        FilterBuilder config
-//
-//        AtmosphereServlet atmosphereServlet = new AtmosphereServlet();
-//        atmosphereServlet.framework().addInitParameter("com.sun.jersey.config.property.packages", "com.adp.resources.atmosphere");
-//        atmosphereServlet.framework().addInitParameter("org.atmosphere.websocket.messageContentType", "application/json");
-//        atmosphereServlet.framework().addInitParameter("org.atmosphere.websocket.maxIdleTime", "120000");
-//        atmosphereServlet.framework().addInitParameter("org.atmosphere.interceptor.HeartbeatInterceptor.heartbeatFrequencyInSeconds", "60");
-//        atmosphereServlet.framework().addInitParameter("org.atmosphere.cpr.AsyncSupport.maxInactiveActivity", "120000");
-//        environment.getApplicationContext().addServlet("org.atmosphere.cpr.AtmosphereServlet", "/socket/*");
-//    }
+
+    void initializeAtmosphere(Environment environment) {
+        AtmosphereServlet servlet = new AtmosphereServlet();
+        servlet.framework().addInitParameter("com.sun.jersey.config.property.packages", "dk.cooldev.chatroom.resources.websocket");
+        servlet.framework().addInitParameter(ApplicationConfig.WEBSOCKET_CONTENT_TYPE, "application/json");
+        servlet.framework().addInitParameter(ApplicationConfig.WEBSOCKET_SUPPORT, "true");
+
+        ServletRegistration.Dynamic servletHolder = environment.servlets().addServlet("Chat", servlet);
+        servletHolder.addMapping("/chat/*");
+    }
 }
